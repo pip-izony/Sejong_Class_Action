688d04493b79e0849e366d5b05fb982353bad8d1
pip-izony <eeodqql09@gmail.com>
2022-08-08 23:43:16 +0900

    Change Sign_up Page

--- src/pages/Sign_up.js
+++ src/pages/Sign_up.js
@@ -50,7 +50,6 @@ const Sign_up = () => {
                             <span class="box int_pass">
                                 <input type="text" id="pswd1" class="int" maxlength="20"/>
                                 <span id="alertTxt">사용불가</span>
-                                <img src="m_icon_pass.png" id="pswd1_img1" class="pswdImg"/>
                             </span>
                             <span class="error_next_box"></span>
                         </div>
@@ -58,7 +57,6 @@ const Sign_up = () => {
                             <h3 class="join_title"><label for="pswd2">비밀번호 재확인</label></h3>
                             <span class="box int_pass_check">
                                 <input type="text" id="pswd2" class="int" maxlength="20"/>
-                                <img src="m_icon_check_disable.png" id="pswd2_img1" class="pswdImg"/>
                             </span>
                             <span class="error_next_box"></span>
                         </div>
