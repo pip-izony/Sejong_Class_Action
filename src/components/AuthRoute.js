525c863a48428c0e729ff3687ed6f4cddd3efbfe
pip-izony <eeodqql09@gmail.com>
2022-08-25 09:11:28 +0900

    Update AuthRoute

--- src/components/AuthRoute.js
+++ src/components/AuthRoute.js
@@ -1,23 +1,11 @@
 import React from "react";
+import {Sign_in, Validation} from "../pages"
 import { Route, Navigate } from "react-router-dom";
 
 function AuthRoute({ authenticated, component: Component, render, ...rest }) {
   return (
     <Route
-      {...rest}
-      render={(props) =>
-        authenticated ? (
-          render ? (
-            render(props)
-          ) : (
-            <Component {...props} />
-          )
-        ) : (
-          <Navigate
-            to={{ pathname: "/log_in", state: { from: props.location } }}
-          />
-        )
-      }
+      path={(props) => authenticated ? Validation : Sign_in} element={<Sign_in/>}
     />
   );
 }
